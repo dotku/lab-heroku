fc996fc8362fd8c4013d1d2bc2be18f0933f8627
Jay Lin <jay.lin@xero.com>
2019-08-08 19:56:29 -0700

    easy list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('hello')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { LoremIpsum } from "lorem-ipsum";
+
+
+const EasyList = ({data, tag: Elem = 'li'}) => {
+  return data.map( (item, idx) => <Elem key={idx}>{item}</Elem>)
+}
+
+function Root() {
+  const liParagraphs = (new LoremIpsum().generateParagraphs(3)).split('\n');
+  const TestTag = 'p';
+  return (
+    <div className={'container'}>
+      <h1>React Root</h1>
+      <EasyList data={liParagraphs} tag="p"/>
+      <TestTag>Test</TestTag>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <Root/>,
+  document.getElementById('root')
+);
